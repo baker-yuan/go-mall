28001a6478ac4bb9312bdd6b3cece3843ce207f0
bakeryuan <bakeryuan@tencent.com>
2023-11-28 12:59:41 +0800

    feat：前端input组件封装优化

--- go-mall-admin/src/components/CommaInput/index.vue
+++ go-mall-admin/src/components/CommaInput/index.vue
@@ -1,3 +1,7 @@
+<!--
+一、Vue 3 Composition API
+-->
+<!--
 <template>
   <el-input :autosize="true" type="textarea" v-model="formattedValue"></el-input>
 </template>
@@ -50,3 +54,96 @@ export default {
   }
 };
 </script>
+-->
+
+<!--
+二、更简洁的 Composition API 写法 （语法糖）
+-->
+<template>
+  <el-input :autosize="true" type="textarea" v-model="formattedValue"></el-input>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect } from "vue";
+import { ElInput } from "element-plus";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: ""
+  }
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const convertValue = (value: string) => {
+  return value.replace(/,/g, "\n");
+};
+const formatValue = (value: string) => {
+  return value.replace(/\n/g, ",");
+};
+
+let formattedValue = ref(convertValue(props.modelValue));
+
+watchEffect(() => {
+  formattedValue.value = convertValue(props.modelValue);
+});
+
+watchEffect(() => {
+  emit("update:modelValue", formatValue(formattedValue.value));
+});
+</script>
+
+<!--
+三、Options API（Vue 2 的写法）
+-->
+<!--
+<template>
+  <el-input :autosize="true" type="textarea" v-model="formattedValue"></el-input>
+</template>
+
+<script lang="ts">
+import { ElInput } from "element-plus";
+
+export default {
+  name: "CommaInput",
+  components: {
+    ElInput
+  },
+  props: {
+    modelValue: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      formattedValue: this.convertValue(this.modelValue)
+    };
+  },
+  methods: {
+    convertValue(value: string) {
+      return value.replace(/,/g, "\n");
+    },
+    formatValue(value: string) {
+      return value.replace(/\n/g, ",");
+    },
+    updateValue(value: string) {
+      this.formattedValue = this.convertValue(value);
+      this.$emit("update:modelValue", this.formatValue(this.formattedValue));
+    }
+  },
+  watch: {
+    modelValue: {
+      handler(newValue) {
+        this.updateValue(newValue);
+      },
+      immediate: true
+    },
+    formattedValue(newValue) {
+      this.$emit("update:modelValue", this.formatValue(newValue));
+    }
+  }
+};
+</script>
+-->
